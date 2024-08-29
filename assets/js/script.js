fac1bce84cf7df6cdff4d69b9842ee85d294a66f
Paula Paris <paulaparis@Air-de-Paula.lan>
2024-08-29 13:09:01 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Seleccionar elementos de html
+const toEmail = document.getElementById("to-email"); //email del destinatario
+const fromEmail = document.getElementById("from-email"); // email del destinatario
+const ccList = document.getElementById("cc-list");  // lista de correos cc
+const sendButton = document.getElementById("send-button"); // boton enviar
+
+// añadir event listener al botón
+sendButton.addEventListener("click", function()){
+    //capturar los correos electrónicos
+}
